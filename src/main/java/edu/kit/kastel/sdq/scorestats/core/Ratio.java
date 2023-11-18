d5f95f0ed11d830df0f0249e0dffb084533a5d8b
Moritz Hertler <mo.hertler@gmail.com>
2023-11-18 10:31:36 +0100

    Add missing ratio record

---
+++ src/main/java/edu/kit/kastel/sdq/scorestats/core/Ratio.java
@@ -0,0 +1,9 @@
+/* Licensed under EPL-2.0 2023. */
+package edu.kit.kastel.sdq.scorestats.core;
+
+/**
+ * @author Moritz Hertler
+ * @version 1.0
+ */
+public record Ratio(double numerator, double denominator) {
+}
